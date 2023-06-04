3fbb64c6b1a9510aea1f537d84016e6eedd43687
branch10480 <toshiharu.imaeda@gmail.com>
2023-06-05 00:00:41 +0900

    Update

--- Sources/MainModule/RootViewController.swift
+++ Sources/MainModule/RootViewController.swift
@@ -12,7 +12,8 @@ public class RootViewController: UIViewController {
 
     public override func viewDidLoad() {
         super.viewDidLoad()
-        setup()
+//        setup()
+        setupTransformer()
     }
 
     public static func instantiate() -> RootViewController {
@@ -21,6 +22,7 @@ public class RootViewController: UIViewController {
 
     private func setup() {
         button.setTitle("ボタン", for: .normal)
+
         var config = UIButton.Configuration.filled()
         config.imagePadding = 8
         config.image = UIImage(systemName: "pencil.slash")
@@ -36,6 +38,35 @@ public class RootViewController: UIViewController {
         config.background = backgroundConfig
 
         button.configuration = config
+    }
+
+    private func setupTransformer() {
+        button.setTitle("ボタン", for: .normal)
+
+        var config = UIButton.Configuration.filled()
+        config.imagePadding = 8
+        config.image = UIImage(systemName: "pencil.slash")
+        config.imagePlacement = .leading
+        config.contentInsets = .init(top: 8, leading: 32, bottom: 8, trailing: 32)
+        config.baseBackgroundColor = .lightGray
+
+        config.imageColorTransformer = .init({ [weak self] _ in
+            guard let self else { return .clear }
+            switch button.state {
+            case .highlighted: return .yellow
+            default: return .tintColor
+            }
+        })
+
+        var backgroundConfig = UIBackgroundConfiguration.clear()
+        backgroundConfig.strokeColor = .black
+        backgroundConfig.strokeWidth = 1.0
+        backgroundConfig.cornerRadius = 5
+
+        config.background = backgroundConfig
+
+        button.configuration = config
+
     }
 
 }
